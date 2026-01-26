fbd4b758530589265f9659f24c3e8a1f6adf1886
Abdelnasser <ahmed.abdelnasser2803@gmail.com>
2026-01-26 14:47:32 +0000

    radio btn

--- tests/uiComponents.spec.js
+++ tests/uiComponents.spec.js
@@ -5,13 +5,10 @@ test.beforeEach(async ({page})=>{
     await page.goto('http://localhost:4200/')
     await page.getByText('Forms').click()
     await page.getByText('Form Layouts').click()
-})
-
-test('From Layout Page', async ({page}) => {
     await page.getByText('Forms').click()
     await page.getByText('Form Layouts').click()
+})
 
-});
 
 test('iput field', async ({page}) => {
 
@@ -32,7 +29,31 @@ test('iput field', async ({page}) => {
     await expect(inputValue).toEqual('test@test.com');
     // check the value by tohavevalue function
     await expect(emailInput).toHaveValue('test@test.com');
-
-
-
 })
+
+test('radio button', async ({page}) => {
+    // declare the usingTheGridForm
+    const usingTheGridForm = page.locator('nb-card' , {hasText: 'Using the Grid'});
+    // check the option1 by label 
+    // await usingTheGridForm.getByLabel('Option 1').check({force:true});
+    // check the option1 by role
+    await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force:true});
+
+    // compare the radio btn is checked or not 
+    // declare the const for status of option 1
+    // check this status is true
+    const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked();
+    await expect(radioStatus).toBeTruthy();
+    // check the option 1 without const variable by tobechecked function
+    await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked();
+
+
+    // if checked option 2 the option 1 should not be checked
+    await usingTheGridForm.getByLabel('Option 2').check({force:true});
+    await expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy();
+    await expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'})).toBeChecked();
+});
+
+test.afterEach(async ({page})=>{
+     await page.close();
+});
